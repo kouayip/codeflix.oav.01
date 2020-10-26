0e5f278df64aff5c2676d40b303dd34afd7c2f89
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-26 15:04:45 +0100

    update and test of the parseENV function

--- main.js
+++ main.js
@@ -10,24 +10,27 @@ const parse = function (filename) {
   const ext = path.extname(filename);
   if (ext.toLowerCase() === EXT_ENV) {
     const contents = fs.readFileSync(filename).toString();
-    parseENV(contents);
+    parseENV(filename, ext);
   } else if (ext.toLowerCase() === EXT_INI) {
-    const contents = fs.readFileSync(filename).toString();
-    parseINIT(contents);
+    parseINIT(filename, ext);
   } else {
     console.error("Error extension file", ext);
   }
 };
 
-const parseINIT = function (fileContents) {};
+const parseINIT = function (fileName, ext) {};
 
-const parseENV = function (fileContents) {
-  const matcher = fileContents.match(REGEX_ENV);
+const parseENV = function (filename, ext) {
+  const fileShotName = filename.replace(ext, "");
+  const contents = fs.readFileSync(filename).toString();
+  const matcher = contents.match(REGEX_ENV);
   const parse = parserValues(matcher);
   const jsonString = JSON.stringify(parse);
-  console.log(jsonString);
+  const filenameGenerate = fileGenerator(fileShotName);
+  createFile(filenameGenerate, jsonString);
 };
 
+// Transform array to object values
 const parserValues = function (tab) {
   const objParse = {};
   for (const str of tab) {
@@ -37,6 +40,23 @@ const parserValues = function (tab) {
     }
   }
   return objParse;
+};
+
+// Generate new filename
+const fileGenerator = function (fileShotName) {
+  const dateYear = new Date().getFullYear();
+  const timestamp = Date.now();
+  const token = `${dateYear}${timestamp}`;
+  const filename = `${fileShotName}.${token}.json`;
+  return filename;
+};
+
+// Create new filename
+const createFile = function (filename, contents) {
+  fs.appendFile(filename, contents, function (err) {
+    if (err) return console.error(err);
+    console.log("`", filename, "`", "has been successfully created");
+  });
 };
 
 parse(process.argv[2]);
