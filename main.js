32e0e490295d884f0e03e8dfa64032053f5a58ed
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-26 14:24:43 +0100

    feature ParserENV to JSON stringify file contents

--- main.js
+++ main.js
@@ -1,37 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-const filename = process.argv[2];
 
 const EXT_ENV = ".env";
 const EXT_INI = ".ini";
 
+const REGEX_ENV = /(.*)=(.*)/gm;
+
 const parse = function (filename) {
   const ext = path.extname(filename);
   if (ext.toLowerCase() === EXT_ENV) {
-    parseENV(filename);
+    const contents = fs.readFileSync(filename).toString();
+    parseENV(contents);
   } else if (ext.toLowerCase() === EXT_INI) {
-    parseINIT(filename);
+    const contents = fs.readFileSync(filename).toString();
+    parseINIT(contents);
   } else {
     console.error("Error extension file", ext);
   }
 };
 
-const parseINIT = function (filename) {
-  fs.readFile(filename, "utf8", function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log(data);
-  });
+const parseINIT = function (fileContents) {};
+
+const parseENV = function (fileContents) {
+  const matcher = fileContents.match(REGEX_ENV);
+  const parse = parserValues(matcher);
+  const jsonString = JSON.stringify(parse);
+  console.log(jsonString);
 };
 
-const parseENV = function (filename) {
-  fs.readFile(filename, "utf8", function (err, data) {
-    if (err) {
-      return console.error(err);
+const parserValues = function (tab) {
+  const objParse = {};
+  for (const str of tab) {
+    const sp = str.split("=");
+    if (sp) {
+      objParse[sp[0]] = sp[1];
     }
-    console.log(data);
-  });
+  }
+  return objParse;
 };
 
 parse(process.argv[2]);
