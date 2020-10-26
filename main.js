2a41edcba570079c868259439d26b498fc046c62
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-26 15:56:45 +0100

    feature file ext init

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const EXT_ENV = ".env";
 const EXT_INI = ".ini";
 
 const REGEX_ENV = /(.*)=(.*)/gm;
+const REGEX_INI = /^([\w.]+)\s{0,}=\s{0,}(.*)/gm;
 
 const parse = function (filename) {
   const ext = path.extname(filename);
@@ -18,25 +19,67 @@ const parse = function (filename) {
   }
 };
 
-const parseINIT = function (fileName, ext) {};
+const parseINIT = function (filename, ext) {
+  const fileShotName = filename.replace(ext, "");
+  const contents = fs.readFileSync(filename).toString();
+  const matcher = contents.match(REGEX_INI);
+  const parse = parserINIValues(matcher.sort());
+  const jsonString = JSON.stringify(parse);
+  const filenameGenerate = fileGenerator(fileShotName);
+  createFile(filenameGenerate, jsonString);
+};
 
 const parseENV = function (filename, ext) {
   const fileShotName = filename.replace(ext, "");
   const contents = fs.readFileSync(filename).toString();
   const matcher = contents.match(REGEX_ENV);
-  const parse = parserValues(matcher);
+  const parse = parserEVENValues(matcher);
   const jsonString = JSON.stringify(parse);
   const filenameGenerate = fileGenerator(fileShotName);
   createFile(filenameGenerate, jsonString);
 };
 
 // Transform array to object values
-const parserValues = function (tab) {
+const parserEVENValues = function (tab) {
   const objParse = {};
   for (const str of tab) {
     const sp = str.split("=");
     if (sp) {
       objParse[sp[0]] = sp[1];
+    }
+  }
+  return objParse;
+};
+
+// Transform array to object values
+const parserINIValues = function (tab) {
+  const objParse = {};
+  for (const str of tab) {
+    const sp = str.split("=");
+    if (sp) {
+      const pf = sp[0].split(".");
+      if (pf.length === 1) {
+        const key = pf[0].trim();
+        objParse[key] = sp[1].trim();
+      } else if (pf.length === 2) {
+        const key1 = pf[0].trim();
+        const key2 = pf[1].trim();
+        if (!objParse[key1]) {
+          objParse[key1] = {};
+        }
+        objParse[key1][key2] = sp[1].trim();
+      } else if (pf.length === 3) {
+        const key1 = pf[0].trim();
+        const key2 = pf[1].trim();
+        const key3 = pf[2].trim();
+        if (!objParse[key1]) {
+          objParse[key1] = {};
+        }
+        if (!objParse[key1][key2]) {
+          objParse[key1][key2] = {};
+        }
+        objParse[key1][key2][key3] = sp[1].trim();
+      }
     }
   }
   return objParse;
